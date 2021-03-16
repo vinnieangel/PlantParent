3f2fd06103e9412ac041119322c239f229495c5d
Vinnie <vinniemadison@gmail.com>
2021-03-15 22:02:13 -0400

    stack navigation

--- App.js
+++ App.js
@@ -4,10 +4,15 @@ import { render } from "react-dom";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./screens/Login";
 import CreateAccount from "./screens/CreateAccount";
+import { createStackNavigator } from "@react-navigation/stack";
 
+const Stack = createStackNavigator();
 export default class App extends React.Component {
   render() {
-    return <Login></Login>;
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="CreateAccount" component={CreateAccount} />
+    </Stack.Navigator>;
   }
 }
 
