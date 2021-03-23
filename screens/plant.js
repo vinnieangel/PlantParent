c258d030c68c612464cfc138cd5a2dedd05ff289
jjlee783 <lee783@wisc.edu>
2021-03-23 11:44:12 +0900

    Update plant.js

--- screens/plant.js
+++ screens/plant.js
@@ -1,71 +1,88 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { TouchableOpacity } from "react-native";
+import Navbar from "./../components/NavBar/NavBar";
+
 
 export default class AmaryllisInfo extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.wrapper}>
-                    <View style={styles.plantDescriptionContainer}>
-                        <Image style={styles.plantImage} source={require("../Images/p2.jpg")} />
-                        <View style={styles.plantDescription}>
-                            <Text style={styles.titleText}>
-                                Amaryllis
-                        </Text>
-                            <Text style={styles.subText}>This is a houseplant.</Text>
+            <ScrollView style={styles.container}>
+                <View style={styles.infoWrapper}>
+                    <View style={styles.plantImage}>
+                        <Image
+                            style={styles.imageContainer}
+                            source={require("../Images/p2.jpg")}
+                        />
+                    </View>
+                    <View style={styles.descriptionWrapper}>
+                        <View>
+                            <Text style={styles.title}>Bush Plant</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.subtitle}>
+                                If the "Swiss Chess Plant", the Monstera delicosa is famous for
+                                its quirky natural leaf holes.
+                            </Text>
                         </View>
                     </View>
                 </View>
-                <View style={styles.categoryContainer}>
-                    <TouchableOpacity
-                        style={styles.categoryButton}
-                        onPress={() => this.props.navigation.navigate("MenuItems")}
-                    >
-                        <View style={styles.categoryIcon}>
+
+                <View style={styles.needFlexWrapper}>
+                    <View style={styles.needContainer}>
+                        <View style={styles.icon}>
                             <Ionicons name="sunny-outline" size={35} color="green" />
+                            <Text style={styles.amount}>Full Sun</Text>
                         </View>
-                        <Text style={styles.categoryButtonTxt}>Full Sun</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={styles.categoryButton}
-                        onPress={() => this.props.navigation.navigate("Order")}
-                    >
-                        <View style={styles.categoryIcon}>
+                        <View style={styles.icon}>
                             <Ionicons name="water-outline" size={35} color="green" />
+                            <Text style={styles.amount}>Every Week</Text>
                         </View>
-                        <Text style={styles.categoryButtonTxt}>Every Week</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={styles.categoryButton}
-                        onPress={() => {
-                            this.props.navigation.navigate("PrintBill");
-                        }}
-                    >
-                        <View style={styles.categoryIcon}>
+                        <View style={styles.icon}>
                             <Ionicons name="resize-outline" size={35} color="green" />
+                            <Text style={styles.amount}>Large</Text>
                         </View>
-                        <Text style={styles.categoryButtonTxt}>Large</Text>
-                    </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.categoryContainer}>
-                    <Text style={styles.titleText}>Care</Text>
-                    <TouchableOpacity
-                        style={styles.categoryButton}
-                        onPress={() => {
-                            this.props.navigation.navigate("PrintBill");
-                        }}
-                    >
-                        <View style={styles.categoryIcon}>
-                            <Ionicons name="chevron-down-outline" size={35} color="green" />
+
+                <View style={styles.careFlexWrapper}>
+                    <View style={styles.careContainer}>
+                        <View style={styles.careWrapper}>
+                            <Text style={styles.careTitle}>Watering</Text>
+                            <Text>Every 5-7 Days</Text>
                         </View>
-                    </TouchableOpacity>
+
+                        <View style={styles.careWrapper}>
+                            <Text style={styles.careTitle}>Repoting</Text>
+                            <Text>Every 6 months</Text>
+                        </View>
+
+                        <View style={styles.careWrapper}>
+                            <Text style={styles.careTitle}>Plant Type</Text>
+                            <Text>Something else</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.careContainer}>
+                        <View style={styles.careWrapper}>
+                            <Text style={styles.careTitle}>Fertilizer Type </Text>
+                            <Text>Organic</Text>
+                        </View>
+
+                        <View style={styles.careWrapper}>
+                            <Text style={styles.careTitle}>Fertilizing Frequency</Text>
+                            <Text>Monthly</Text>
+                        </View>
+
+                        <View style={styles.careWrapper}>
+                            <Text style={styles.careTitle}>Plant size</Text>
+                            <Text>Wow</Text>
+                        </View>
+                    </View>
                 </View>
-            </View>
+
+                {/* <Navbar/> */}
+            </ScrollView >
         );
     }
 }
@@ -74,67 +91,83 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "white",
+        flexDirection: "column",
+        marginRight: 30,
+        marginLeft: 30
     },
-    titleText: {
-        fontFamily: "Helvetica",
-        fontSize: 20,
-        paddingRight: 50,
-        color: "black",
-        paddingBottom: 20,
+    careContainer: {
+        flexDirection: "column",
+        justifyContent: "flex-start",
     },
-    subText: {
-        textAlign: "left",
-        fontSize: 18,
-        paddingBottom: 40,
+    careWrapper: {
+        flexDirection: "column",
+        justifyContent: "space-around",
+        paddingBottom: 15
     },
-    categoryContainer: {
+    careFlexWrapper: {
+        display: "flex",
         flexDirection: "row",
+        justifyContent: "space-between",
+        paddingLeft: 25,
+        paddingRight: 25
+    },
+    needContainer: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
         width: "90%",
-        alignSelf: "center",
         marginTop: 25,
         marginBottom: 10,
     },
-    categoryIcon: {
-        borderWidth: 0,
+    icon: {
+        display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        alignSelf: "center",
         width: 70,
         height: 70,
-        borderRadius: 50,
     },
-    categoryButton: {
-        flex: 1,
-        width: "30%",
-        marginHorizontal: 0,
-        alignSelf: "center",
-    },
-    categoryButtonTxt: {
-        alignSelf: "center",
+    amount: {
+        fontSize: 10,
         marginTop: 5,
         color: "black",
     },
-    // logoContainer: {
-    //     paddingTop: 40,
-    // },
+    infoWrapper: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
     plantImage: {
         flex: 1,
-        width: 100,
+        alignItems: "center",
+    },
+    imageContainer: {
+        width: 120,
         height: 200,
         resizeMode: "contain",
     },
-    plantDescription: {
-        display: "flex",
+    descriptionWrapper: {
+        flex: 1,
         flexDirection: "column",
-        justifyContent: "start",
+        justifyContent: "flex-start",
     },
-    plantDescriptionContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "start",
+    title: {
+        fontFamily: "Helvetica",
+        fontSize: 20,
+        color: "black",
+        paddingBottom: 10,
     },
-    wrapper: {
+    subtitle: {
+        fontFamily: "Helvetica",
+        textAlign: "left",
+        fontSize: 17,
+    },
+    careTitle: {
+        fontSize: 10,
+        color: "#515151",
+    },
+    needFlexWrapper: {
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "center"
     }
 });
